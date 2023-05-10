24da67bedd3efc3238598d0d0ccafda7de02ab5c
Giannis Fardelas <farg245@gmail.com>
2023-05-10 17:17:30 +0300

    pagination added to backend

--- src/backend/routes/home.js
+++ src/backend/routes/home.js
@@ -17,20 +17,26 @@ const axios = require('axios');
 // fetchBitcoinMarketData();
 
 
-
-
 router.get('/', async (req, res) => {
     try {
+        const  page  = req.query.page;
+        const PAGE_SIZE = req.query.PAGE_SIZE
+        const pageNumber = parseInt(page, 10) || 1;
+        const startIndex = (pageNumber - 1) * PAGE_SIZE;
+        const endIndex = pageNumber * PAGE_SIZE;
+
         const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en');
-        // const data=response.data[100]
-    //     const parsedData = {
-    //     name: data.name,
-    //     symbol: data.symbol,
-    //     current_price: data.current_price,
-    //     market_cap: data.market_cap,
-    //     price_change_percentage_24h: data.price_change_percentage_24h,
-    //   };
-      res.json(response.data);
+        const data = response.data.slice(startIndex, endIndex);
+        // const data=response.data
+        const parsedData = data.map(item => ({
+            name: item.name,
+            symbol: item.symbol,
+            current_price: item.current_price,
+            highest_price : item.high_24h,
+            lowest_price : item.low_24h,
+            price_change_24h: item.price_change_24h,
+          }));
+      res.json(parsedData)
     } catch (error) {
       console.error(error);
       res.status(500).send('Error retrieving data from GeckoCoin API');
